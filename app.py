0efec75ae5d56ee59f601a6f57e4d862dc9ac2ee
seanthony <sean@basecampcodingacademy.org>
2019-08-30 09:56:31 -0500

    hello world, this is the first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+# Import modules here!
+
+SERVICES_FILENAME = "./services.py"
+TRANSACTIONS_FILENAME = "./transactions.py"
+
+
+def print_welcome_message():
+    print("""
+                        .--~~,__
+            :-....,-------`~~'._.'
+            `-,,,  ,_      ;'~U'
+            _,-' ,'`-__; '--.
+            (_/'~~      ''''(;
+ WELCOME TO LUXURY DOG SERVICES!
+    """)
+
+
+def dog_spa():
+    print_welcome_message():
+        # Keep writing code here!
+
+
+if __name__ == '__main__':
+    dog_spa()
